7a5daef7628d0d872cc560b90f3382bcb6b7df65
Karnilov <125124458+Karnilov@users.noreply.github.com>
2024-10-13 14:45:52 +0700

    Update and rename LGP/adding/comm/LGP.py to plugin/LGP/settings.py

---
+++ plugin/LGP/settings.py
@@ -0,0 +1,5 @@
+#                       -----LGP-----
+#	sys	bsod	scc	appp
+cores={"8":[["sys"],["sys"],["bsod"],["scc"],["app"],["app"],["asyncio"],["asyncio"]}
+bust_sys=(False,{"3":["sys"],"4":["bsod","scc"]})
+bust_sys=(False,{"3":["bsod","scc"],"4":["app"]})
