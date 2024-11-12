71a1c5b61273146848b5cdcd2c5b2aa5a8e26fb2
Karnilov <125124458+Karnilov@users.noreply.github.com>
2024-11-12 18:14:21 +0700

    Update core.py

--- plugin/core/classes/core.py
+++ plugin/core/classes/core.py
@@ -1,7 +1,6 @@
 import settings.core as settings
 import threading
 import time
-
 def find_shortest_list(tr, cores, target_type):
     indices = [i for i, core in enumerate(cores["8"]) if core[0] == target_type]
     shortest_list = None
@@ -12,19 +11,20 @@ def find_shortest_list(tr, cores, target_type):
             shortest_length = len(current_list)
             shortest_list = index
     return shortest_list
-    
-
 class app:
     time=0
     is_alive=False
     time=0
     pause=False
     stop=False
-    
+    type='app'
+    name='app'
+    init=False
     def __init__(self):
         self.tread=threading.Thread(target=self.work, daemon=True)
-        
     def work(self):
+        if self.init:
+            self.Init()
         while not self.stop:
             if not self.pause:
                 t1=time.time_ns()
@@ -35,16 +35,17 @@ class app:
                     
     def Cycle(self):
         pass
-        
+    def Init(self):
+        pass
     def start(self):
-        self.tread.start()
         self.stop=False
+        self.init=True
+        self.tread.start()
     def join(self):self.tread.join()
     def timeout(self): return self.time
     def answer(self): return self.is_alive
     def terminate(self):self.stop=True
-    
-    
+    def load(self):return (self.timeout()/1000000)/0.1*100
 class core:
     cores=[[],[],[],[],[],[],[],[]]
     def add_process(self, func, type):
@@ -54,16 +55,15 @@ class core:
     def kill(self,CID, PID):
         self.cores[CID][PID].terminate()
         del self.cores[CID][PID]
-
 class scc:
     def __init__(self,core):
         self.core=core
-        self.millis=20
+        self.millis=0.1
     def regulation(self):
         max=0
         while True:
             for cores_index, cores_ in enumerate(self.core.cores):
                 for core_index, core_ in enumerate(cores_):
                     t=core_.timeout()/1000000
-                    if t>self.millis:
+                    if t>self.millis or core_.stop:
                         self.core.kill(cores_index,core_index)
