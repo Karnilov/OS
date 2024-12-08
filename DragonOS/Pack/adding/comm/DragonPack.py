3a5c8d2f3f3214bd91f18eaf3fd9e5f51bde881c
Karnilov <125124458+Karnilov@users.noreply.github.com>
2024-12-08 20:03:48 +0700

    Update DragonPack.py

--- DragonOS/Pack/adding/comm/DragonPack.py
+++ DragonOS/Pack/adding/comm/DragonPack.py
@@ -1,7 +1,16 @@
-
+from comm import git
+from main import run
 def install(flag="-c"):
+	text=open('run.py').read()
+	if not 'import kivy' in text:
+		text='import kivy\n'+text
+	open('run.py', "w").write(text)
 	match flag:
 		case '-c':
-			print('instaling DragonConsole')
+			git('https://github.com/Karnilov/OS/blob/main/DragonOS/Console/install')
+			run('install/install.comm')
+		case '-c':
+			git('https://github.com/Karnilov/OS/blob/main/DragonOS/OS/install')
+			run('install/install.comm')
 		case _:
-			print('not flag')
+			print("Err index")
